02b463d24da463f29c30329d07e01e574ce3b826
joe goodman <jgood1133@gmail.com>
2023-03-23 16:44:17 -0400

    ice dragon

--- cow.py
+++ cow.py
@@ -37,3 +37,12 @@ class Dragon(Cow):
     def can_breathe_fire(self):
         return True
 
+class IceDragon(Dragon):
+    def __init__(self, name, image):
+        super().__init__(name, image)
+
+    # does this need to take in self?
+    # could be static??
+    def can_breathe_fire(self):
+        return False
+
