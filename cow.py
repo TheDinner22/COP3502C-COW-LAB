09bad6b01caf7f35f7274b45826f3af40648660f
joe goodman <jgood1133@gmail.com>
2023-03-23 16:42:38 -0400

    dragon class

--- cow.py
+++ cow.py
@@ -27,3 +27,13 @@ class Cow:
         print(msg)
         print(self.image)
 
+class Dragon(Cow):
+    def __init__(self, name, image):
+        super().__init__(name)
+        self.image = image
+
+    # does this need to take in self?
+    # could be static??
+    def can_breathe_fire(self):
+        return True
+
