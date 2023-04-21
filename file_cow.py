c403c74923c0dbc8e791e24bf87486652b06648e
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2023-04-20 21:18:27 -0400

    class impl

--- file_cow.py
+++ file_cow.py
@@ -1,8 +1,20 @@
+import os
 from cow import Cow
 
+# assumes the filename is valid
+def read_file_to_string_or_error(filename):
+    path = os.path.join("cows", filename)
+
+    try:
+        with open(path) as file_object:
+            return file_object.read()
+    except Exception:
+        raise RuntimeError("MOOOOO!!!!!!")
+
 class FileCow(Cow):
-    def __init__(self, name):
+    def __init__(self, name, filename):
         super().__init__(name)
+        self.image = read_file_to_string_or_error(filename)
 
     def set_image(self, image):
         raise RuntimeError("Cannot reset FileCow Image")
