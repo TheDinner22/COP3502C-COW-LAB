907e6122699b37ef716d847f26a09d9a8886ad4b
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2023-04-20 20:55:17 -0400

    removed confusing comment

--- cow.py
+++ cow.py
@@ -27,34 +27,3 @@ class Cow:
         print(msg)
         print(self.image)
 
-"""
-class Dragon(Cow):
-    def __init__(self, name, image):
-        super().__init__(name)
-        self.image = image
-
-    # does this need to take in self?
-    # could be static??
-    def can_breathe_fire(self):
-        return True
-
-    def print(self, msg):
-        assert self.image != None, "this cow was not initilized properly"
-
-        # can the dragon breath fire or not?
-        can_or_cannot = "can" if self.can_breathe_fire() else "cannot"
-
-        print(msg)
-        print(self.image)
-        print(f"This dragon {can_or_cannot} breathe fire.")
-
-class IceDragon(Dragon):
-    def __init__(self, name, image):
-        super().__init__(name, image)
-
-    # does this need to take in self?
-    # could be static??
-    def can_breathe_fire(self):
-        return False
-"""
-
