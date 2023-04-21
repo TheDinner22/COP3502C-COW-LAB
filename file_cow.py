b165f5f4f66154f09f8899019e0c0a7211ab4af0
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2023-04-20 21:13:57 -0400

    first method

--- file_cow.py
+++ file_cow.py
@@ -3,3 +3,6 @@ from cow import Cow
 class FileCow(Cow):
     def __init__(self, name):
         super().__init__(name)
+
+    def set_image(self, image):
+        raise RuntimeError("Cannot reset FileCow Image")
