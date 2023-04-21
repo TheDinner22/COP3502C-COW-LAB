ea88e7ea2ae4c65ff9a4ac4dcc0c397dca85314c
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2023-04-20 21:00:41 -0400

    new heif_gen file

--- heifer_generator.py
+++ heifer_generator.py
@@ -1,6 +1,12 @@
 from cow import Cow
 from dragon import Dragon
 from ice_dragon import IceDragon
+from file_cow import FileCow
+import os
+
+# This iteration of HeiferGenerator contains two useful functions:
+# get_cows() returns a list of Cows, Dragons, IceDragons
+# get_file_cows() returns a list of FileCows
 
 class HeiferGenerator:
 
@@ -43,6 +49,7 @@ class HeiferGenerator:
 					"              ///-._ _ _ _ _ _ _}^ - - - - ~                   `-----'\n"
 
 	cows = None
+	file_cows = None
 
 	@staticmethod
 	def get_cows():
@@ -52,7 +59,7 @@ class HeiferGenerator:
 			num_regular = len(HeiferGenerator.cow_names)
 			for index in range(num_regular):
 				HeiferGenerator.cows[index] = Cow(HeiferGenerator.cow_names[index])
-				HeiferGenerator.cows[index].image = HeiferGenerator.quote_lines + HeiferGenerator.cowImages[index]
+				HeiferGenerator.cows[index].set_image(HeiferGenerator.quote_lines + HeiferGenerator.cowImages[index])
 			
 			# add the dragons
 			for index in range(len(HeiferGenerator.dragon_names)):
@@ -62,3 +69,21 @@ class HeiferGenerator:
 				)
 			
 		return HeiferGenerator.cows
+	
+	@staticmethod
+	def filter(filename):
+		return filename.endswith('.cow')
+
+	@staticmethod
+	def get_file_cows():
+		if HeiferGenerator.file_cows is None:
+			HeiferGenerator.file_cows = []
+			# loop through 'cows/' directory, if it is a valid file then add it to the list
+			# from https://www.geeksforgeeks.org/how-to-iterate-over-files-in-directory-using-python/
+			for filename in os.listdir('cows'):		# for zybooks, replace 'cows' with '.'
+				if HeiferGenerator.filter(filename):
+					# for zybooks, replace os.path.join('cows', filename) with filename
+					c = FileCow(filename[0:-4], os.path.join('cows', filename))
+					HeiferGenerator.file_cows.append(c)
+
+		return HeiferGenerator.file_cows
